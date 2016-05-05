106636246f38a9203a9078b9dff3e428e12071da
snowmi <trouvelot@gmail.com>
2016-05-05 17:28:42 +0900

    Improve sample.rb

--- sample.rb
+++ sample.rb
@@ -8,6 +8,16 @@ first_rental = Rental.new(jaws, 3)
 second_rental = Rental.new(et, 5)
 third_rental = Rental.new(anpanman, 2)
 
-snowmi = Customer.new("Takahiro")
+snowmi = Customer.new("Snowmi")
+
 snowmi.add_rental(first_rental)
-snowmi.statement
+puts "----- Only first_rental -----"
+puts snowmi.statement
+
+snowmi.add_rental(second_rental)
+puts "----- Add second_rental -----"
+puts snowmi.statement
+
+snowmi.add_rental(third_rental)
+puts "----- Add third_rental -----"
+puts snowmi.statement
