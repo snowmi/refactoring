a85daf8eb9fc7b328b04640eba2097c74341005e
snowmi <trouvelot@gmail.com>
2016-05-08 23:22:06 +0900

    Finish p.37

--- samplecode.rb
+++ samplecode.rb
@@ -22,6 +22,23 @@ class Rental
   def initialize(movie, days_rented)
     @movie, @days_rented = movie, days_rented
   end
+
+  # 金額を計算
+  def charge
+    result = 0
+    case movie.price_code
+    when Movie::REGULAR
+      result += 2
+      result += (days_rented - 2 ) * 1.5 if days_rented > 2
+    when Movie::NEW_RELEASE
+      result += days_rented * 3
+    when Movie::CHILDRENS
+      result += 1.5
+      result += (days_rented - 3 ) * 1.5 if days_rented > 3
+    end
+    result
+  end
+
 end
 
 # ----- Customerクラス -----
@@ -60,21 +77,8 @@ class Customer
     result
   end
 
-  # 金額を計算
   def amount_for(rental)
-    result = 0
-
-    case rental.movie.price_code
-    when Movie::REGULAR
-      result += 2
-      result += (rental.days_rented - 2 ) * 1.5 if rental.days_rented > 2
-    when Movie::NEW_RELEASE
-      result += rental.days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (rental.days_rented - 3 ) * 1.5 if rental.days_rented > 3
-    end
-    result
+    rental.charge
   end
-  
+
 end
