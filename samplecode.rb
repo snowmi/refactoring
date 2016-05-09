dc02eafd69ac53b9cccb7f0e56ca4823b2e0a21b
snowmi <trouvelot@gmail.com>
2016-05-09 21:07:21 +0900

    Finish p.59

--- samplecode.rb
+++ samplecode.rb
@@ -12,6 +12,26 @@ class Movie
   def initialize(title, price_code)
     @title, @price_code = title, price_code
   end
+
+  def charge(days_rented)
+    result = 0
+    case price_code
+    when REGULAR
+      result += 2
+      result += (days_rented - 2) + 1.5 if days_rented > 2
+    when NEW_RELEASE
+      result += days_rented * 3
+    when CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    result
+  end
+
+  def frequent_renter_points(days_rented)
+    (price_code == NEW_RELEASE && days_rented > 1 ) ? 2 : 1
+  end
+
 end
 
 
@@ -25,22 +45,11 @@ class Rental
 
   # 金額を計算
   def charge
-    result = 0
-    case movie.price_code
-    when Movie::REGULAR
-      result += 2
-      result += (days_rented - 2 ) * 1.5 if days_rented > 2
-    when Movie::NEW_RELEASE
-      result += days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (days_rented - 3 ) * 1.5 if days_rented > 3
-    end
-    result
+    movie.charge(days_rented)
   end
 
   def frequent_renter_points
-    (movie.price_code == Movie::NEW_RELEASE && days_rented > 1 ) ? 2 : 1
+    movie.frequent_renter_points(days_rented)
   end
 
 end
